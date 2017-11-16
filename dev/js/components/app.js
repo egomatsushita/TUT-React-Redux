77dd6aa468452974399a708d813f0c31626d939e
egomatsushita <eduardo.go.rj@gmail.com>
2017-11-16 12:18:37 -0500

    Seting up mapStateToProps function

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 require('../../scss/style.scss');
+import UsersList from '../containers/users-list.js';
 
 const App = () => (
   <div>
     <h2>Username List:</h2>
+    <UsersList />
     <hr />
     <h2>User Details:</h2>
   </div>
--- dev/js/index.js
+++ dev/js/index.js
@@ -14,11 +14,18 @@ import App from './components/app.js';
 // 6. create index.js inside reducers directory to combine all the reducers in a big object
 // 7. import all combined reducers (allReducers) from ./reducers/index.js
 // 8. create a components directory inside js directory
-// 9. create a App.js inside components directory
+// 9. create a app.js inside components directory
 // 10. import Provider object from react-redux
 // 11. import App from ./components/app.js
 // 12. render the Provider tag with an store attribute set to store value. This store will be avaiable to all components
 // 13. add App tag inside Provider tag
+// 14. create containers directory inside js directory
+// 15. create users-list.js inside containers directory
+// 16. import UsersList from /containers/users-list.js at /components/app.js
+// 17. add mapStateToProps function in /cotainers/users-list.js 
+//     it takes a peace of application store and passes it to UsersList component as a property
+// 18. in the users-list.js, instead of exporting UsersList object, connect mapStateToProps to UserList
+//     now, the UsersList component is aware of application store
 
 const store = createStore(allReducers); 
 
