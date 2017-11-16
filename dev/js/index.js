58ccb1d6975135adb09e06b54d3b19a4bae4eef6
egomatsushita <eduardo.go.rj@gmail.com>
2017-11-16 10:37:10 -0500

    Seting up store

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux' // 1. wil only import the function createStore from redux
+
+// 3. a reducer is a function that says what data is supposed to save in our store
+// 4. create a reducers dir
+// 5. create a reducer-users.js inside reducers dir
+
+const store = createStore(); // 2. store is all the data of the entire application
 
 ReactDOM.render(<h1>Hey now</h1>, document.getElementById('root'));
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,7 @@
+export default function() { // will return an array of users
+  return [
+    {
+
+    }
+  ]
+}
